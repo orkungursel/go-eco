139221a2defcc0c15bf9bd16c5144ad8a43a7550
Orkun Gürsel <orkungrsl@gmail.com>
2022-04-06 00:11:24 +0300

    feat: add global api for the package

---
+++ global.go
@@ -0,0 +1,31 @@
+package eco
+
+var ee *eco
+
+func init() {
+	ee = New()
+}
+
+func SetPrefix(prefix string) *eco {
+	return ee.SetPrefix(prefix)
+}
+
+func SetArraySeparator(sep string) *eco {
+	return ee.SetArraySeparator(sep)
+}
+
+func SetEnvNameTransformer(transformerFunc envNameTransformerFunc) *eco {
+	return ee.SetEnvNameTransformer(transformerFunc)
+}
+
+func SetEnvNameSeparator(envNameSeparator string) *eco {
+	return ee.SetEnvNameSeparator(envNameSeparator)
+}
+
+func SetValueGetter(valueGetter envValueGetterFunc) *eco {
+	return ee.SetValueGetter(valueGetter)
+}
+
+func Unmarshal(model any) error {
+	return ee.Unmarshal(model)
+}
